e5982df595be1dfd9cb2fd0ced8dc146dcbdc5af
Nicolás Salvo <nicosalvo@gmail.com>
2024-08-27 02:17:18 +0000

    refactor: remove comments

--- main.go
+++ main.go
@@ -5,17 +5,6 @@ import (
 	"task-tracker/stores"
 )
 
-/*
-TODO: refactor as using done/todo/in-progress as boolean flags
-# Listing all tasks
-task-cli list
-
-# Listing tasks by status
-task-cli list done
-task-cli list todo
-task-cli list in-progress
-*/
-
 func main() {
 	jsonStore := stores.NewJsonTaskStore("tasks.json")
 	commandLine := services.NewCommandLine(jsonStore)
